7505ae4915a8aa39a488809e889298d4e96cf505
Eddie Crona <ecrona@gmail.com>
2018-06-21 10:46:14 +0200

    Removed console log, closes #18, closes #15

--- src/features/login/store/actions.ts
+++ src/features/login/store/actions.ts
@@ -3,7 +3,6 @@ import { LoginMethod } from '../models/login-method'
 
 export function authenticate(loginMethod: LoginMethod): ThunkAction {
   return async (dispatch, getState, firebase) => {
-    console.log(firebase, loginMethod)
     switch (loginMethod) {
       case LoginMethod.Google:
         firebase.authenticate()
