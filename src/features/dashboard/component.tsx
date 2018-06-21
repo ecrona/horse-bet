bdf4fe028d21eb93ba4d19331083061bac08a018
Vibo <vibo92@hotmail.com>
2018-06-22 00:13:23 +0200

    Fix responsive tables, closes #23

--- src/features/dashboard/component.tsx
+++ src/features/dashboard/component.tsx
@@ -15,79 +15,95 @@ import green from '@material-ui/core/colors/green'
 import { Placement } from 'models/placement'
 import { Winner } from 'models/winner'
 import { BetModal } from './components/bet-modal'
+import { WithStyles, Theme, withStyles } from '@material-ui/core'
 
-interface Props extends StoreProps {}
+interface Props
+  extends StoreProps,
+    WithStyles<keyof ReturnType<typeof styles>> {}
 
-export default class Component extends React.PureComponent<Props> {
-  render() {
-    return (
-      <div
-        style={{
-          maxWidth: '1280px',
-          padding: '20px',
-          margin: '0 auto'
-        }}
-      >
-        <Grid container spacing={16}>
-          {this.props.stages.map((stage, index) => (
-            <Grid key={index} item xs={12}>
-              <Paper>
-                <Toolbar>
-                  <Typography variant="title" id="tableTitle">
-                    {stage.name}
-                  </Typography>
-                </Toolbar>
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Match</TableCell>
-                      {this.props.users.map((user, index) => (
-                        <TableCell key={index}>{user.name}</TableCell>
-                      ))}
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {stage.fixtures.map((fixture, index) => (
-                      <TableRow key={index}>
-                        <TableCell
-                          component="th"
-                          scope="row"
-                          style={{ width: '250px', cursor: 'pointer' }}
-                          onClick={() => this.props.openBetModal(fixture)}
-                        >
-                          {fixture.home} - {fixture.away}
-                        </TableCell>
-                        {fixture.placements.map((placement, index) => (
-                          <TableCell key={index} style={{ width: '150px' }}>
-                            {placement === Placement.Home && fixture.home}
-                            {placement === Placement.Away && fixture.away}
-                            {placement === Placement.NotPlaced && (
-                              <Clear color="secondary" />
-                            )}
-                            {placement === Placement.Placed && (
-                              <Done color="primary" />
-                            )}
-                          </TableCell>
-                        ))}
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </Paper>
-            </Grid>
-          ))}
-        </Grid>
-        <BetModal
-          state={this.props.betModalState}
-          homeTeam={this.props.selectedFixture.home}
-          awayTeam={this.props.selectedFixture.away}
-          selectedBet={this.props.selectedBet}
-          placeBet={(winner: Winner) =>
-            this.props.placeBet(this.props.selectedFixture, winner)
-          }
-          onClose={this.props.closeBetModal}
-        />
-      </div>
-    )
+const styles = (theme: Theme) => ({
+  tableWrapper: {
+    overflow: 'auto hidden',
+    '& th:first-child': {
+      minWidth: '225px'
+    }
   }
-}
+})
+
+export default withStyles(styles)(
+  class Component extends React.PureComponent<Props> {
+    render() {
+      return (
+        <div
+          style={{
+            maxWidth: '1280px',
+            padding: '20px',
+            margin: '0 auto'
+          }}
+        >
+          <Grid container spacing={16}>
+            {this.props.stages.map((stage, index) => (
+              <Grid key={index} item xs={12}>
+                <Paper>
+                  <Toolbar>
+                    <Typography variant="title" id="tableTitle">
+                      {stage.name}
+                    </Typography>
+                  </Toolbar>
+                  <div className={this.props.classes.tableWrapper}>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Match</TableCell>
+                          {this.props.users.map((user, index) => (
+                            <TableCell key={index}>{user.name}</TableCell>
+                          ))}
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {stage.fixtures.map((fixture, index) => (
+                          <TableRow key={index}>
+                            <TableCell
+                              component="th"
+                              scope="row"
+                              style={{ width: '250px', cursor: 'pointer' }}
+                              onClick={() => this.props.openBetModal(fixture)}
+                            >
+                              {fixture.home} - {fixture.away}
+                            </TableCell>
+                            {fixture.placements.map((placement, index) => (
+                              <TableCell key={index} style={{ width: '150px' }}>
+                                {placement === Placement.Home && fixture.home}
+                                {placement === Placement.Away && fixture.away}
+                                {placement === Placement.NotPlaced && (
+                                  <Clear color="secondary" />
+                                )}
+                                {placement === Placement.Placed && (
+                                  <Done color="primary" />
+                                )}
+                              </TableCell>
+                            ))}
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </div>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+          <BetModal
+            state={this.props.betModalState}
+            homeTeam={this.props.selectedFixture.home}
+            awayTeam={this.props.selectedFixture.away}
+            selectedBet={this.props.selectedBet}
+            placeBet={(winner: Winner) =>
+              this.props.placeBet(this.props.selectedFixture, winner)
+            }
+            onClose={this.props.closeBetModal}
+          />
+        </div>
+      )
+    }
+  }
+)
