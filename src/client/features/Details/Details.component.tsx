8499ebc7bba00a21d769490467e42bfd1a6aeac2
Vibo <vibo92@hotmail.com>
2019-02-07 23:44:22 +0100

    Fix details view

--- src/client/features/Details/Details.component.tsx
+++ src/client/features/Details/Details.component.tsx
@@ -3,28 +3,32 @@ import styles from './Details.styles.scss'
 import { Toolbar } from '@client/shared/components/Toolbar/component'
 import { StoreProps } from './Details.container'
 import { BetPlacement } from '@client/../shared/models/bet-placement'
+import { Fixture } from '../Dashboard/components/Fixture'
 
 export class Details extends React.PureComponent<StoreProps> {
   render() {
-    console.log(this.props.fixture)
+    const { fixture } = this.props
+    console.log(fixture)
 
     return (
       <React.Fragment>
         <Toolbar subtitle="Game details" canGoBack hideHighscore />
 
         <div className={styles.container}>
-          <div>
-            <img src={this.props.fixture.homeTeam.logo} />
+          <div className={styles.team}>
+            <span className={styles.teamName}>{fixture.homeTeam.name}</span>
+            <img className={styles.logo} src={fixture.homeTeam.logo} />
           </div>
 
-          <div>{this.props.fixture.score}</div>
+          <div className={styles.score}>{fixture.score || '-'}</div>
 
-          <div>
-            <img src={this.props.fixture.awayTeam.logo} />
+          <div className={styles.team}>
+            <span className={styles.teamName}>{fixture.awayTeam.name}</span>
+            <img className={styles.logo} src={fixture.awayTeam.logo} />
           </div>
         </div>
 
-        {this.props.fixture.bets.map(bet => (
+        {fixture.bets.map(bet => (
           <div>
             {bet.name} -> {bet.placement}
           </div>
