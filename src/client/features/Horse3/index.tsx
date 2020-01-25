fe4cba75dec471c6e51e6b99c717599e27ed8d41
Vibo <vibo92@hotmail.com>
2020-01-25 20:16:29 +0100

    WIP impl

--- src/client/features/Horse3/index.tsx
+++ src/client/features/Horse3/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Toolbar } from '@client/shared/components/Toolbar/component'
+import LockIcon from '@material-ui/icons/lock'
+
+import './styles.scss'
 
 export default function Horse3() {
   return (
     // Background etc
-    <div className="bg-gray-200 h-full">
+    <div className="bg-gray-200">
       <div className="pb-12">Tullbar here</div>
 
-      <div className="bg-gray-300 pt-6 px-6 pb-12 text-yellow-300 relative">
+      <div className="bg-gray-300 pt-6 px-6 pb-10 text-yellow-300 relative">
         <span className="block font-medium">Gneigh,</span>
-        <span className="block font-extrabold text-xl">Piotr Sköldström</span>
+        <span className="block font-bold text-2xl">Piotr Sköldström</span>
 
         <div
           className="bg-purple-300 text-2xl font-bold rounded-full absolute right-0 bottom-0 mr-6 -mb-8 shadow text-white flex items-center justify-center"
@@ -32,31 +35,62 @@ export default function Horse3() {
       <div className="pb-6"></div>
 
       <div className="pb-10">
-        <div className="shadow relative">
-          <div
-            className="bg-gray-100 px-6 text-white flex items-center hover:bg-yellow-300 hover:text-gray-300"
-            style={{ height: 64 }}
-          >
+        <div className="bet-fixture">
+          <div className="bet-button">
             <img
-              style={{ width: 48, height: 48 }}
+              className="bet-button__logo"
               src="/assets/logotypes/real.png"
             />
 
             <span className="pl-3 font-medium">Real Madrid</span>
           </div>
 
-          <div style={{ height: 1, backgroundColor: '#3d3d3d' }}></div>
+          <div className="bet-fixture__separator"></div>
 
-          <div
-            className="bg-gray-100 px-6 text-white flex items-center hover:bg-yellow-300 hover:text-gray-300"
-            style={{ height: 64 }}
-          >
+          <div className="bet-button">
             <img
-              style={{ width: 48, height: 48 }}
+              className="bet-button__logo"
               src="/assets/logotypes/roma.png"
             />
 
             <span className="pl-3 font-medium">Roma</span>
+          </div>
+
+          <div className="bet-fixture__state">
+            <span className="text-white">vs</span>
+          </div>
+        </div>
+
+        <footer className="pt-3 text-center">
+          <span
+            style={{
+              fontFamily: 'rockwell',
+              fontSize: '12px',
+              color: 'rgba(255,255,255,0.75)'
+            }}
+          >
+            View details
+          </span>
+        </footer>
+      </div>
+
+      <div className="pb-10">
+        <div className="bet-fixture">
+          <div className="bg-yellow-300 px-6 text-gray-300 flex items-center">
+            <img className="bet-button__logo" src="/assets/logotypes/psg.png" />
+
+            <span className="pl-3 font-medium">PSG</span>
+          </div>
+
+          <div className="bet-fixture__separator"></div>
+
+          <div className="bet-button">
+            <img
+              className="bet-button__logo"
+              src="/assets/logotypes/tottenham.png"
+            />
+
+            <span className="pl-3 font-medium">Tottenham</span>
           </div>
 
           <div
@@ -86,27 +120,18 @@ export default function Horse3() {
       </div>
 
       <div className="pb-10">
-        <div className="shadow relative">
-          <div
-            className="bg-gray-100 px-6 text-white flex items-center hover:bg-yellow-300 hover:text-gray-300"
-            style={{ height: 64 }}
-          >
-            <img
-              style={{ width: 48, height: 48 }}
-              src="/assets/logotypes/psg.png"
-            />
+        <div className="bet-fixture">
+          <div className="bg-yellow-300 px-6 text-gray-300 flex items-center">
+            <img className="bet-button__logo" src="/assets/logotypes/psg.png" />
 
             <span className="pl-3 font-medium">PSG</span>
           </div>
 
-          <div style={{ height: 1, backgroundColor: '#3d3d3d' }}></div>
+          <div className="bet-fixture__separator"></div>
 
-          <div
-            className="bg-gray-100 px-6 text-white flex items-center hover:bg-yellow-300 hover:text-gray-300"
-            style={{ height: 64 }}
-          >
+          <div className="bet-button" style={{ height: 64, opacity: 0.5 }}>
             <img
-              style={{ width: 48, height: 48 }}
+              className="bet-button__logo"
               src="/assets/logotypes/tottenham.png"
             />
 
@@ -123,6 +148,112 @@ export default function Horse3() {
             }}
           >
             <span className="text-white">vs</span>
+          </div>
+        </div>
+
+        <footer className="pt-3 text-center">
+          <span
+            style={{
+              fontFamily: 'rockwell',
+              fontSize: '12px',
+              color: 'rgba(255,255,255,0.75)'
+            }}
+          >
+            View details
+          </span>
+        </footer>
+      </div>
+
+      <div className="pb-10">
+        <div className="bet-fixture">
+          <div className="bg-red-100 px-6 text-white flex items-center">
+            <img className="bet-button__logo" src="/assets/logotypes/psg.png" />
+
+            <span className="pl-3 font-medium">PSG</span>
+          </div>
+
+          <div className="bet-fixture__separator"></div>
+
+          <div className="bet-button" style={{ height: 64, opacity: 0.5 }}>
+            <img
+              className="bet-button__logo"
+              src="/assets/logotypes/tottenham.png"
+            />
+
+            <span className="pl-3 font-medium">Tottenham</span>
+          </div>
+
+          <div
+            className="absolute bg-gray-300 rounded-full shadow flex items-center justify-center"
+            style={{
+              height: 36,
+              width: 36,
+              right: 64,
+              top: 'calc(64px - (36px / 2))'
+            }}
+          >
+            <span className="text-white">vs</span>
+          </div>
+        </div>
+
+        <footer className="pt-3 text-center">
+          <span
+            style={{
+              fontFamily: 'rockwell',
+              fontSize: '12px',
+              color: 'rgba(255,255,255,0.75)'
+            }}
+          >
+            View details
+          </span>
+        </footer>
+      </div>
+
+      <div className="pb-10">
+        <div className="bet-fixture">
+          <div className="bg-green-100 px-6 text-white flex items-center">
+            <img className="bet-button__logo" src="/assets/logotypes/psg.png" />
+
+            <span className="pl-3 font-medium flex-1">PSG</span>
+
+            <span
+              className="pl-2"
+              style={{ fontFamily: 'rockwell', justifySelf: 'end' }}
+            >
+              3
+            </span>
+          </div>
+
+          <div className="bet-fixture__separator"></div>
+
+          <div className="bet-button" style={{ height: 64, opacity: 0.5 }}>
+            <img
+              className="bet-button__logo"
+              src="/assets/logotypes/tottenham.png"
+            />
+
+            <span className="pl-3 font-medium flex-1">Tottenham</span>
+
+            <span
+              className="pl-2"
+              style={{ fontFamily: 'rockwell', justifySelf: 'end' }}
+            >
+              1
+            </span>
+          </div>
+
+          <div
+            className="absolute bg-gray-300 rounded-full shadow flex items-center justify-center"
+            style={{
+              height: 36,
+              width: 36,
+              right: 64,
+              top: 'calc(64px - (36px / 2))'
+            }}
+          >
+            <span className="text-white">
+              <LockIcon style={{ width: 24 }} />
+            </span>
           </div>
         </div>
 
