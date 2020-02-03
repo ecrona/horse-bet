21a1ea9238c15a54646eb1ae3137c84d914d4607
Vibo <vibo92@hotmail.com>
2020-02-03 18:21:06 +0100

    WIP

--- src/client/features/Details/index.tsx
+++ src/client/features/Details/index.tsx
@@ -9,7 +9,32 @@ export default function Details({}: Props) {
     <>
       <Toolbar />
 
-      <div>Info</div>
+      <div className="pb-10"></div>
+
+      <div className="px-6 pb-6 text-white">
+        <h3 className="text-xl font-bold">Fixture details</h3>
+      </div>
+
+      <div className="p-6 bg-gray-200"></div>
+      <div className="p-6 bg-gray-400"></div>
+
+      <div className="pb-6"></div>
+
+      <div className="px-6">
+        <span
+          className="text-yellow-300 font-bold"
+          style={{ fontFamily: 'Rockwell' }}
+        >
+          Who picked what
+        </span>
+      </div>
+
+      <div
+        className="bg-purple-300 text-yellow-300 flex items-center"
+        style={{ height: 36 }}
+      >
+        76%
+      </div>
     </>
   )
 }
