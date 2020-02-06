14666e716f1fb9f4e2d732d83d270182ed4c2cf5
Vibo <vibo92@hotmail.com>
2020-02-06 23:40:24 +0100

    Add search to highscore. Styling changes

--- src/client/features/Highscore/index.tsx
+++ src/client/features/Highscore/index.tsx
@@ -1,5 +1,5 @@
 import Toolbar from '@client/shared/components/Toolbar'
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchHighscores } from '../OldHighscore/store/actions'
 import { getHighscores, getMyHighscore } from '../OldHighscore/store/selectors'
@@ -10,9 +10,19 @@ export default function Highscore() {
   const highscores = useSelector(getHighscores)
   const myHighscore = useSelector(getMyHighscore)
 
+  const [query, setQuery] = useState('')
+  const filteredHighscores = useMemo(
+    () => highscores.filter(highscore => highscore.name.includes(query)),
+    [highscores, query]
+  )
+
   useEffect(() => {
     dispatch(fetchHighscores())
   }, [])
+
+  function handleChangeQuery(event) {
+    setQuery(event.target.value)
+  }
 
   if (!highscores || !highscores.length || !myHighscore) return null
 
@@ -20,15 +30,17 @@ export default function Highscore() {
     <>
       <Toolbar hideHighscore />
 
-      <div className="bg-gray-400 pt-6 px-6 pb-8 text-yellow-300 relative">
-        <span className="block font-medium">Gneigh,</span>
-        <span className="block font-bold text-2xl">{myHighscore.name}</span>
-        <span className="block text-sm font-extrabold text-white">
-          {myHighscore.score} pts
-        </span>
+      <div className="flex bg-gray-400 pt-6 px-6 pb-8 text-yellow-300 items-center">
+        <div className="flex-1">
+          <span className="block font-medium">Gneigh,</span>
+          <span className="block font-bold text-2xl">{myHighscore.name}</span>
+          <span className="block text-sm font-extrabold text-white">
+            You have a massive {myHighscore.score} pts
+          </span>
+        </div>
 
         <div
-          className="bg-purple-300 text-2xl font-bold rounded-full absolute right-0 bottom-0 mr-6 -mb-8 shadow text-white flex items-center justify-center"
+          className="bg-purple-300 text-2xl font-bold rounded-full mr-6 shadow text-white flex items-center justify-center"
           style={{ height: 64, width: 64, fontFamily: 'rockwell' }}
         >
           {myHighscore.rank}
@@ -38,9 +50,14 @@ export default function Highscore() {
         </div>
       </div>
 
-      <div className="pb-10"></div>
+      <input
+        className="highscore-search"
+        placeholder="Search for a better..."
+        value={query}
+        onChange={handleChangeQuery}
+      />
 
-      {highscores.map(highscore => (
+      {filteredHighscores.map(highscore => (
         <div key={highscore.name} className="highscore-item">
           <span className="highscore-item__rank">{highscore.rank}</span>
           <span className="highscore-item__name">{highscore.name}</span>
